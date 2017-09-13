6ea4ec28d07a709e62f430829bc06def575a3311
Saurabh londhe <saurabhlondhe1111@gmail.com>
2017-09-13 23:14:11 +0530

    Update webExtractor.py
    added new choices

--- webExtractor.py
+++ webExtractor.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python2
-#Made by Saurabh Londhe
+#made by SAURABH LONDHE
 import os
 import urllib2
-url=raw_input("Enter url= http://")
-url=("http://"+url)
-test = urllib2.urlopen(url).read()
+os.system("tput clear")
+#-------------------------------------------------------------------------------
+print "Base url means it must contain string till its domain it should not be extended with its pages or file extensions\nSuch as;\n\t\t'http://ritindia.edu'\n\n('http://ritindia.edu/index.html')such url is not valid\n"
+print "both base url and url must be of same website" 
+print "*************************************************************\n"
+print "Use (https://website_name.com) format 'www.web_name.com' will give error\n'"
+url=raw_input("Enter base url= ")
+os.system("tput clear")
+print "\ncopy link from browser it may or may not contain /name.html extensions\n"
+base_url=raw_input("Enter page url= ")
+#-------------------------------------------------------------------------------
+
+test = urllib2.urlopen(base_url).read()
 test2=test
 os.system("touch web.html")
 data=open("web.html","w")
@@ -12,7 +22,7 @@ data=open("web.html","w")
 data.write(test)
 data.close()
 #---------------------------------------------------------------------------------
-choice=int(input("Enter method\n\t1)href\n\t2)src"))
+choice=int(input("Enter method\n\t1)href\n\t2)src\n\t\t="))
 if choice==1:
 	sane = 0
 	needlestack = []
@@ -26,12 +36,15 @@ if choice==1:
 		test = test[curpos+1:testlen]
 		curpos = test.find('"')
 		needle = test[0:curpos]
-		if needle.startswith("http" or "www" or "/"):#and needle.endswith(".js"):
+		if needle.startswith("http" or "www" or "/"):
 			needlestack.append(needle)
 		elif needle.startswith(""):
-			needlestack.append(url+needle)
+			needlestack.append(url+"/"+needle)
 	  else:
 		sane = 1
+		for item in needlestack:
+			dwnld=item
+			print dwnld
 #--------------------------------------------------------------------------------
 elif choice==2:
 	sane = 0
@@ -46,12 +59,23 @@ elif choice==2:
 		test = test[curpos+1:testlen]
 		curpos = test.find('"')
 		needle = test[0:curpos]
-		if needle.startswith("http" or "www" or "/"):#and needle.endswith(".js"):
+		if needle.startswith("http" or "www" or "/"):
 			needlestack.append(needle)
 		elif needle.startswith(""):
-			needlestack.append(url+needle)
+			needlestack.append(url+"/"+needle)
 	  else:
 		sane = 1
+		for item in needlestack:
+			dwnld=item
+			print dwnld
 #--------------------------------------------------------------------------------
-for item in needlestack:
-  print item
+ch=raw_input("\nDo you Want to Download(Y/N)")
+if ch=='y' or ch=='Y':	
+	os.system("rm -rf swap")
+	os.system("mkdir swap")
+	for item in needlestack:
+		
+		dwnld="cd swap && "+"wget "+item
+	#	print dwnld
+		os.system(dwnld)
+	#  print item
